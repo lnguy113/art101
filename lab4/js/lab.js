cd90e808195176bff7dff14958aac529a352b8c3
lnguy113 <lnguy113@ucsc.edu>
2023-04-20 08:53:57 -0700

    uploading lab 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,64 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - pseudocoding and problem-solving 
+// Author: Lily Nguyen
+// Date: 18 April 2023
+
+
+/* 
+Task 1: Pseudocode an everyday task 
+  1. Create a list of high-level tasks
+  2. If necessary, break complicated task down into sub-steps
+  3. Put this in outline form
+
+  high-level tasks 
+  - making coffee 
+  - deep cleaning the bathroom
+  - cooking pasta 
+
+  sub-steps 
+    making coffee 
+      1. take out the coffee powder and scoop out a small amount into a seperate cup
+      2. make some hot water
+      3. once the water is hot, pour about 1/4 a cup into the cup with coffee powder 
+      4. take a spoon and mix throughly
+      5. add milk and creamer (amount may vary due to liking)
+
+Task 2: Pseudocode a computer task 
+  1. create a list of high-level tasks your computer program is going to have to do to be complete
+  2. keep it high level 
+  3. Break down complicated task into sub-tasks
+
+  outline 
+    creating an exam counter
+      1. set pass, fails, student to either 0 or 1
+      2. check the students score
+      3. if less than passing number, add to fail
+      4. if higher than passing number, add to pass
+      5. prints final number
+
+Task 3: Turn pseudocode into comments 
+  author's block 
+    // summary.
+    // description. 
+    // @link URL 
+    // @file This file includes the psuedocode for my exam counter
+    // @author Lily Nguyen 
+    // @since 4.18.23
+
+  psuedocode: creating an exam counter
+    // initalize pass to zero 
+    // initalize fail to zero 
+    // initalize students to one
+    // while students counter is less than or equal to ten
+      // input the next exam results 
+      // if the students passed 
+        // add one to passes counter 
+      // else 
+        // add ones to fails counter
+    // add one to student counter 
+    // print the number of passes
+    // print the number of fails
+*/
+
 
 // Constants
 
@@ -19,3 +77,6 @@ function main() {
 
 // let's get this party started
 main();
+
+
+   
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,18 +1,41 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Lily Nguyen 
+// Date: 20 April 
 
 // Constants
 
 // Functions
 
+// Define Variables 
+make = "Honda";
+model = "Accord Sport";
+color = "Olive";
+year = 2014;
+
+// calculate 
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>"); 
+document.writeln("Model: " + model + "<br>"); 
+document.writeln("Color: " + color + "<br>"); 
+document.writeln("Year: " + year + "<br>"); 
+document.writeln("Age: " + age + " years<br>"); 
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+function myFunction(param1, param2) 
+{
   // some code here
   // return results;
+  document.writeln("Make: " + make + "<br>"); 
+  document.writeln("Model: " + model + "<br>"); 
+  document.writeln("Color: " + color + "<br>"); 
+  document.writeln("Year: " + year + "<br>"); 
+  document.writeln("Age: " + age + " years<br>"); 
 }
 
-function main() {
+function main() 
+{
   console.log("Main function started.");
   // the code that makes everything happen
 }
