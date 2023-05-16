7109e79b1da9b522d7205e773bd05d4ae75dbf77
lnguy113 <lnguy113@ucsc.edu>
2023-05-15 19:10:35 -0700

    uploading lab 10

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -6,73 +6,36 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function sortUserName()
+function sortUserName(userName) 
 {
-  // some code here
-  // return results;
+  // split the array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
 
-  // //nameSort() from lab 7
-  // var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-  // console.log("userName -", userName);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
 
-  // // split atring to array 
-  // var nameArray = userName.split(''); 
-  // console.log("nameArray -", nameArray);
-
-  // // sort the array
-  // var nameArraySort = nameArray.sort();
-  // console.log("nameArraySort -", nameArraySort);
- 
-  // join array back to a string 
+  // join back to a string
   var nameSorted = nameArraySort.join('');
-  console.log("nameSorted -", nameSorted);
+  console.log("nameSorted =", nameSorted);
 
-  var photoEl = document.getElementById("user-name");
-  var buttonEl = document.createElement("my-button");
-
-  buttonEl.innerHTML = "Press me!";
-  photoEl.appendChild(buttonEl);
-
-  buttonEl.addEventListener("click", function()
-  {
-    alert("Hi");
-  })
-  return nameSorted; 
+  //  userName.toLower().split("").sort().join("")
+  return nameSorted;
 }
 
-const inputField = document.getElementById('user-name');
-const outputDiv = document.getElementById('output');
+// adding button
+var button = document.getElementById('my-button');
 
-document.getElementById('my-button').addEventListener('click', function()
+button.addEventListener('click', function() 
 {
-  const inputValue = inputField.ariaValueMax;
-  const results = sort(inputValue);
-  
-  outputDiv.innerHTML = results;
+  console.log("We got here.");
+  inputValue = document.getElementById('user-name').value;
+  var sortedName = sortUserName(inputValue);
+  console.log("Results", sortedName);
+  document.getElementById("output").innerHTML = sortedName;
 });
 
-function sort(str)
-{
-  const sortedArr = str.split('').sort();
-  return sortedArr.join('');
-}
-
-function anagram(atr)
-{
-  const arr = str.split('');
-  for (let i = arr.length - 1; i > 0; i--)
-  {
-    const j = Math.floor(Math.random() * (i + 1));
-
-    [arr[i], arr[j]] = [arr[i], arr[j]];
-  }
-  return arr.join('');
-}
-
-// Output 
-// document.writeln("Oh hey, I've fixed your name: ", 
-//   sortUserName(), "</br>");
 
 function myFunction(param1, param2) {
   // some code here
